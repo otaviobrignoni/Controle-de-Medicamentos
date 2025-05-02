a4fb387730c1f4de443a9aa5ed8d92e9fa7ec4d1
AlexAraldi <alexanderaraldi@gmail.com>
2025-05-02 20:30:29 -0300

    Feat: implementado Repositorio Paciente.

---
+++ Controle-de-Medicamentos.ConsoleApp/PatientModule/IPatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using Controle_de_Medicamentos.ConsoleApp.Shared.BaseModule;
+namespace Controle_de_Medicamentos.ConsoleApp.PatientModule
+{
+    public interface IPatientRepository : IRepository<Patient>
+    {
+
+    }
+
+
+}
+
+
