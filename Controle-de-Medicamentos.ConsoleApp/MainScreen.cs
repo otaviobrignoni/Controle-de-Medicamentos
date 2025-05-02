01e6846cc16792fb187fce1099b5ff94d625275a
Agatha Sates <satesagatha@gmail.com>
2025-05-02 04:28:31 -0300

    feat: Implementado Menu Principal

---
+++ Controle-de-Medicamentos.ConsoleApp/MainScreen.cs
@@ -0,0 +1,70 @@
+﻿using Controle_de_Medicamentos.ConsoleApp.Shared;
+using Controle_de_Medicamentos.ConsoleApp.MedicationModule;
+using Controle_de_Medicamentos.ConsoleApp.Utils;
+using Controle_de_Medicamentos.ConsoleApp.Shared.BaseModule;
+
+
+namespace Controle_de_Medicamentos.ConsoleApp;
+
+public class MainScreen
+{
+    // declaração de variáveis repositorio
+    private string option;
+    private DataContext context;
+    private IMedicationRepository medicamentoRepository;
+
+    // declarar as Telas
+    private MedicationScreen medicationScreen;
+
+    public MainScreen()
+    {
+        // intanciar Repositorios de cada entidade e telas
+        context = new DataContext(true);
+        medicamentoRepository = new MedicationRepository(context);
+        medicationScreen = new MedicationScreen(medicamentoRepository, "Medicamento");
+    }
+
+    public void ShowMainMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Write.Header("Controle de Medicamentos");
+            Console.WriteLine();
+            Write.WriteInColor(" [1] - Fornecedores", ConsoleColor.Cyan);
+            Write.WriteInColor(" [2] - Pacientes", ConsoleColor.Cyan);
+            Write.WriteInColor(" [3] - Funcionários", ConsoleColor.Cyan);
+            Write.WriteInColor(" [4] - Medicamentos", ConsoleColor.Cyan);
+            Write.WriteInColor(" [5] - Prescrições Médicas", ConsoleColor.Cyan);
+            Write.WriteInColor(" [6] - Requisição de Entrada", ConsoleColor.Cyan);
+            Write.WriteInColor(" [7] - Requisição de Saída", ConsoleColor.Cyan);
+            Write.WriteInColor(" [8] - Sair", ConsoleColor.Cyan);
+            Console.WriteLine();
+            Write.WriteInColor(">> Digite a opção desejada: ", ConsoleColor.Yellow, true);
+            option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1": return;
+                case "2": return;
+                case "3": return;
+                case "4": medicationScreen.ShowMenu(); break;
+                case "5": return;
+                case "6": return;
+                case "7": return;
+                case "8": ShowLeaveMessage(); return;
+                default: Write.ShowInvalidOptionMessage(); ShowMainMenu(); break;
+            }
+        }
+    }
+
+    public void ShowLeaveMessage()
+    {
+        Console.Clear();
+        Write.WriteInColor("╔══════════════════════════════════════════════╗", ConsoleColor.DarkCyan);
+        Write.WriteInColor("║ Obrigado por usar o Contole de Medicamentos! ║", ConsoleColor.DarkCyan);
+        Write.WriteInColor("║              Até a próxima!                  ║", ConsoleColor.DarkCyan);
+        Write.WriteInColor("╚══════════════════════════════════════════════╝", ConsoleColor.DarkCyan);
+    }
+
+}
--- Controle-de-Medicamentos.ConsoleApp/Program.cs
+++ Controle-de-Medicamentos.ConsoleApp/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MainScreen MainScreen = new MainScreen();
+            MainScreen.ShowMainMenu();
         }
     }
 }
