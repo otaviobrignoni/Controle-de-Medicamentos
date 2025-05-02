8af7b022ab1089f7c82c23354fda14f36ee5858e
AlexAraldi <alexanderaraldi@gmail.com>
2025-05-02 15:52:35 -0300

    Feat: Implementado Supplier Repository.

---
+++ Controle-de-Medicamentos.ConsoleApp/SupplierModule/ISupplierRepository.cs
@@ -0,0 +1,9 @@
+﻿
+using Controle_de_Medicamentos.ConsoleApp.Shared.BaseModule;
+
+namespace Controle_de_Medicamentos.ConsoleApp.SupplierModule
+{
+    public interface ISupplierRepository : IRepository<Supplier>
+    {
+    }
+}
---
+++ Controle-de-Medicamentos.ConsoleApp/SupplierModule/SupplierRepository.cs
@@ -0,0 +1,25 @@
+﻿using Controle_de_Medicamentos.ConsoleApp.Shared;
+using Controle_de_Medicamentos.ConsoleApp.Shared.BaseModule;
+
+namespace Controle_de_Medicamentos.ConsoleApp.SupplierModule
+{
+    public class SupplierRepository : BaseRepository<Supplier>, ISupplierRepository
+    {
+        public SupplierRepository(DataContext context) : base(context) { }
+
+        public override List<Supplier> GetList()
+        {
+            return Context.Suppliers;
+        }
+        public override bool IsEntityValid(Supplier entity, out string errors)
+        {
+            errors = entity.Validate();
+            if (entity.IsSameCNPJ(GetAll().FirstOrDefault(s=> s.IsSameCNPJ(entity))))
+                errors += "Já existe um fornecedor com este CNPJ";
+            if (string.IsNullOrEmpty(errors))
+                return true;
+            return false;
+        }
+    }
+}
+
