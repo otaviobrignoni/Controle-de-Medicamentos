6553e6d807826d6db768f93a9a1d68f21f578678
Agatha Sates <satesagatha@gmail.com>
2025-05-04 01:09:12 -0300

    feat: Implementado repositorio de Prescrição Medica

---
+++ Controle-de-Medicamentos.ConsoleApp/MedicalPrescriptionModule/IMedicalPrescriptionRepository.cs
@@ -0,0 +1,6 @@
+﻿using Controle_de_Medicamentos.ConsoleApp.MedicalPrescriptionsModule;
+using Controle_de_Medicamentos.ConsoleApp.Shared.BaseModule;
+
+namespace Controle_de_Medicamentos.ConsoleApp.MedicalPrescriptionModule;
+
+public interface IMedicalPrescriptionRepository : IRepository<MedicalPrescription>;
---
+++ Controle-de-Medicamentos.ConsoleApp/MedicalPrescriptionModule/MedicalPrescriptionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Controle_de_Medicamentos.ConsoleApp.MedicalPrescriptionsModule;
+using Controle_de_Medicamentos.ConsoleApp.Shared;
+using Controle_de_Medicamentos.ConsoleApp.Shared.BaseModule;
+
+namespace Controle_de_Medicamentos.ConsoleApp.MedicalPrescriptionModule;
+
+public class MedicalPrescriptionRepository : BaseRepository<MedicalPrescription>, IMedicalPrescriptionRepository
+{
+    public MedicalPrescriptionRepository(DataContext context) : base(context) {}
+
+    public override List<MedicalPrescription> GetList()
+    {
+        return Context.MedicalPrescriptions;
+    }
+}
