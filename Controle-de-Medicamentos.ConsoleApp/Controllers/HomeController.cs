bb622dff572aafd2cb9eeb9988a690b09c69b17f
otaviobrignoni <otavio.678.b@gmail.com>
2025-05-27 16:47:08 -0300

    feat: Finalizado estilo para fornecedores, notificações e página inicial

--- Controle-de-Medicamentos.ConsoleApp/Controllers/HomeController.cs
+++ Controle-de-Medicamentos.ConsoleApp/Controllers/HomeController.cs
@@ -9,5 +9,6 @@ public class HomeController : Controller
     public IActionResult HomePage()
     {
         return View();
+        
     }
 }
--- Controle-de-Medicamentos.ConsoleApp/Utils/Write.cs
+++ Controle-de-Medicamentos.ConsoleApp/Utils/Write.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
+﻿
 namespace Controle_de_Medicamentos.ConsoleApp.Utils;
 
 public static class Write
